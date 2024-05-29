41726aa68582af58755d8420cc89c1fff97c91f3
hugomacielads <hugomacielcesar@gmail.com>
2024-05-28 22:23:08 -0300

    feat : Adicionando busca individual /filmes/{id}

--- src/main/java/com/olabi/olabiflix/controller/FilmeController.java
+++ src/main/java/com/olabi/olabiflix/controller/FilmeController.java
@@ -2,9 +2,11 @@ package com.olabi.olabiflix.controller;
 
 import com.olabi.olabiflix.model.entity.Filme;
 import com.olabi.olabiflix.repository.FilmeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/filmes")
@@ -21,9 +23,22 @@ public class FilmeController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Filme getFilmeById(@PathVariable UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new FilmeNotFoundException(id));
+    }
+
     @PostMapping("/criar")
     public Filme create(@RequestBody Filme filmeBody){
         return repository.save(filmeBody);
     }
 
+}
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class FilmeNotFoundException extends RuntimeException {
+    public FilmeNotFoundException(UUID id) {
+        super("Não foi possível encontrar o filme com id: " + id);
+    }
 }
