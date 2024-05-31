580a989d60b6a36bb85a0726381fa4d0524b16ef
hugomacielads <hugomacielcesar@gmail.com>
2024-05-30 22:37:29 -0300

    feat : Adicionando Rotas da Entidade Serie

--- src/main/java/com/olabi/olabiflix/controller/SerieController.java
+++ src/main/java/com/olabi/olabiflix/controller/SerieController.java
@@ -2,9 +2,13 @@ package com.olabi.olabiflix.controller;
 
 import com.olabi.olabiflix.model.entity.Serie;
 import com.olabi.olabiflix.repository.SerieRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/series")
@@ -22,10 +26,26 @@ public class SerieController {
     }
 
     @GetMapping("/{id}") // GET - Busca por Id
+    public Optional<Serie> getById(@PathVariable UUID id){
+        return repository.findById(id);
+    }
 
     @GetMapping("/busca-title") // GET - Busca por título
+    public ResponseEntity<Serie> findByTitle(@RequestParam(name = "title", defaultValue = "") String title) {
+        Optional<Serie> serieEncontrada = repository.findByTitle(title);
+        if (serieEncontrada.isPresent()) {
+            Serie serie = serieEncontrada.get();
+            return ResponseEntity.ok(serie);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
+    // Atributo Genero na Entidade Serie é uma lista de strings, talvez não funciona corretamente
     @GetMapping("/busca-genero") // GET - Busca por gênero
+    public ResponseEntity<List<Serie>> findByGenre(@RequestParam(name = "genre", defaultValue = "") String genre) {
+        List<Serie> series = repository.findByGenreContainsIgnoreCase(genre);
+        return ResponseEntity.ok(series);
+    }
 
     @PostMapping("/criar") // POST - Cadastro de Serie
     public Serie create(@RequestBody Serie serieBody){
@@ -33,4 +53,9 @@ public class SerieController {
     }
 
     @DeleteMapping("/{id}/delete") // DELETE - Exclusão de Filmes
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id){
+        repository.deleteById(id);
+    }
 }
+
--- src/main/java/com/olabi/olabiflix/repository/SerieRepository.java
+++ src/main/java/com/olabi/olabiflix/repository/SerieRepository.java
@@ -1,9 +1,18 @@
 package com.olabi.olabiflix.repository;
 
+import com.olabi.olabiflix.model.entity.Filme;
 import com.olabi.olabiflix.model.entity.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SerieRepository extends JpaRepository<Serie, UUID> {
+
+    Optional<Serie> findByTitle(String title); // Case Sensitive
+
+    List<Serie> findByGenreContainsIgnoreCase(String genre);
+
+    List<Serie> findByTitleOrActors(String title, String actors);
 }
